2b80a124c4f48cf11b88ec2eaae34780f892e58f
Angelo <angjelin003@gmail.com>
2026-09-08 13:26:07 +0200

    Migrate to Android application plugin and Kotlin
    Updated plugins for Android application and Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,4 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
-    application
-}
-
-group = "com.visualizer"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("org.processing:core:3.3.7")
-}
-
-application {
-    mainClass.set("com.visualizer.MainKt")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
