09666d637333a3f89afe9dc3e006734d9c892f92
Angelo <angjelin003@gmail.com>
2026-09-08 18:14:27 +0200

    Clean up commented code in MainActivity
    Removed commented code for switching views in MainActivity.

--- app/src/main/kotlin/com/visualizer/MainActivity.kt
+++ app/src/main/kotlin/com/visualizer/MainActivity.kt
@@ -2,13 +2,10 @@ package com.visualizer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.visualizer.experiments.ExperimentalMorphView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Switch views by changing the line below:
         setContentView(SuperellipsoidView(this))
-        // setContentView(ExperimentalMorphView(this))
     }
 }
