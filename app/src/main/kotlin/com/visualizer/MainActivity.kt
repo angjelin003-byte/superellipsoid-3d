543b22f994bed319af034e4de8dbb362dae5d42c
Angelo <angjelin003@gmail.com>
2026-09-08 18:00:37 +0200

    Refactor view setup in MainActivity
    Updated the MainActivity to switch between SuperellipsoidView and ExperimentalMorphView.

--- app/src/main/kotlin/com/visualizer/MainActivity.kt
+++ app/src/main/kotlin/com/visualizer/MainActivity.kt
@@ -1,22 +1,14 @@
 package com.visualizer
 
 import android.os.Bundle
-import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import processing.android.PFragment
+import com.visualizer.experiments.ExperimentalMorphView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val frame = FrameLayout(this).apply {
-            id = FrameLayout.generateViewId()
-        }
-        setContentView(frame)
-
-        val sketch = SuperellipsoidVisualizer()
-        val fragment = PFragment(sketch)
-        fragment.setView(frame, this)
+        // Switch views by changing the line below:
+        setContentView(SuperellipsoidView(this))
+        // setContentView(ExperimentalMorphView(this))
     }
 }
